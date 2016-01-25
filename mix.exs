7f7d35b7b7758493ed885f625f5168a0713a4d6e
Connor Lay <connor.lay@me.com>
2016-01-24 18:48:26 -0800

    Configure escript to package issues.

--- mix.exs
+++ mix.exs
@@ -5,6 +5,7 @@ defmodule Issues.Mixfile do
     [app: :issues,
      version: "0.0.1",
      elixir: "~> 1.2",
+     escript: escript_config,
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps]
@@ -19,5 +20,9 @@ defmodule Issues.Mixfile do
       { :httpoison, "~> 0.8.0" },
       { :jsx,       "~> 2.0"   }
     ]
+  end
+
+  def escript_config do
+    [ main_module: Issues.CLI ]
   end
 end
